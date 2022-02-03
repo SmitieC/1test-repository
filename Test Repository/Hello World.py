2b48c9f9d3a78ccda6c9b3dcf49efdf159d8c17a
SmitieC <83625219+SmitieC@users.noreply.github.com>
2022-02-04 11:55:33 +1300

    Update Hello World.py

--- Test Repository/Hello World.py
+++ Test Repository/Hello World.py
@@ -1,1 +1,7 @@
+number1 = int(input("Please enter a number: "))
+number2 = int(input("Please enter another number: "))
 
+if number1 == number2:
+    print("The numbers are equal")
+else:
+    print("The higher number is: ", max(number1, number2))
