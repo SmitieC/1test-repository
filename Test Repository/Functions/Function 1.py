ec59788450bcd41cbbe102a689b77bdfb67382e2
SmitieC <83625219+SmitieC@users.noreply.github.com>
2022-02-14 13:56:55 +1300

    wifeui

---
+++ Test Repository/Functions/Function 1.py
@@ -0,0 +1,9 @@
+def add_numbers():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    return num1 + num2
+
+
+# main
+print(add_numbers())
+print(add_numbers())
